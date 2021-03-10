fd4dd3f83e632727ebdde89fdd52f5514f22bd52
Johnny <leejohnny47@gmail.com>
2021-03-09 19:46:04 -0500

    feat: add controller to list available grocery items

---
+++ src/main/java/com/kata/checkout_order/controllers/ListAvailableItemsController.java
@@ -0,0 +1,8 @@
+package com.kata.checkout_order.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListAvailableItemsController {
+    // TODO
+}
