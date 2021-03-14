7e6926ba1e55048ffef663c06ce5955f4f67854b
Johnny <leejohnny47@gmail.com>
2021-03-13 23:24:29 -0500

    feat: add UTs for query controller

--- src/test/java/com/kata/checkout_order/controllers/QueryControllerTest.java
+++ src/test/java/com/kata/checkout_order/controllers/QueryControllerTest.java
@@ -1,5 +1,7 @@
 package com.kata.checkout_order.controllers;
 
+import com.google.gson.Gson;
+import com.kata.checkout_order.entities.GroceryItem;
 import com.kata.checkout_order.repositories.GroceryItemRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.kata.checkout_order.constants.GroceryItemConstants.AVAILABLE_GROCERY_ITEMS;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,6 +27,8 @@ public class QueryControllerTest {
     @MockBean
     private GroceryItemRepository repository;
 
+    private final Gson gson = new Gson();
+
     @Test
     public void getTotal_returnsValidTotal() throws Exception {
         float expectedTotal = 10.0F;
@@ -29,10 +37,16 @@ public class QueryControllerTest {
     }
 
     @Test
-    public void getItems_returnsValidItems() {
+    public void getItems_returnsValidItems() throws Exception {
+        List<GroceryItem> expectedItems = new ArrayList<>();
+        expectedItems.add(AVAILABLE_GROCERY_ITEMS.get(0));
+        when(repository.getItems()).thenReturn(expectedItems);
+        this.mockMvc.perform(get("/query/items")).andExpect(status().isOk()).andExpect(content().json(gson.toJson(expectedItems)));
     }
 
     @Test
-    public void getAvailableItems_returnsValidItems() {
+    public void getAvailableItems_returnsValidItems() throws Exception {
+        when(repository.getAvailableGroceryItems()).thenReturn(AVAILABLE_GROCERY_ITEMS);
+        this.mockMvc.perform(get("/query/available-items")).andExpect(status().isOk()).andExpect(content().json(gson.toJson(AVAILABLE_GROCERY_ITEMS)));
     }
 }
