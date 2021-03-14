04401fb670e690b605fd1f42fc47141a29e60ced
Johnny <leejohnny47@gmail.com>
2021-03-13 22:45:16 -0500

    feat: add stubbed methods for query controller

--- src/main/java/com/kata/checkout_order/controllers/QueryController.java
+++ src/main/java/com/kata/checkout_order/controllers/QueryController.java
@@ -1,12 +1,31 @@
 package com.kata.checkout_order.controllers;
+import com.kata.checkout_order.entities.GroceryItem;
 import com.kata.checkout_order.repositories.GroceryItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/query")
 public class QueryController extends BaseController {
     QueryController(GroceryItemRepository repository) {
         super(repository);
+    }
+
+    @GetMapping("/total")
+    float getTotal() {
+        return 0;
+    }
+
+    @GetMapping("/items")
+    Collection<GroceryItem> getItems() {
+        return null;
+    }
+
+    @GetMapping("/available-items")
+    Collection<GroceryItem> getAvailableGroceryItems() {
+        return null;
     }
 }
